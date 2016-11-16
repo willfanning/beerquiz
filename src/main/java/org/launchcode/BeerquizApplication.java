1d668501246d11d628ae919ae47369b427019825
Will <william.fanning1@gmail.com>
2016-11-16 13:22:05 -0600

    initial commit

---
+++ src/main/java/org/launchcode/BeerquizApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerquizApplication.class, args);
+	}
+}
