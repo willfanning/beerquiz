79b268aabd0ff9a1f15898e33a7a1b33fe1d3cea
Will <william.fanning1@gmail.com>
2016-11-16 16:33:40 -0600

    add crypto dependency

--- src/main/java/org/launchcode/beerquiz/models/User.java
+++ src/main/java/org/launchcode/beerquiz/models/User.java
@@ -1,5 +1,18 @@
 package org.launchcode.beerquiz.models;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "users")
+public class User extends AbstractEntity {
+
+	private String username;
+	private String pwHash;
+	
+	public User(String username, String password) {
+		
+	}
+	
+	public User() {}
 }
