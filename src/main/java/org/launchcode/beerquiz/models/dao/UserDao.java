4b5d6cc6991604a3726249db77dd8139977b8f74
Will <william.fanning1@gmail.com>
2016-11-16 18:50:06 -0600

    outline userDao

--- src/main/java/org/launchcode/beerquiz/models/dao/UserDao.java
+++ src/main/java/org/launchcode/beerquiz/models/dao/UserDao.java
@@ -1,5 +1,21 @@
 package org.launchcode.beerquiz.models.dao;
 
-public class UserDao {
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.launchcode.beerquiz.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Transactional
+@Repository
+public interface UserDao extends CrudRepository<User, Integer> {
+
+    User findByUid(int uid);
+    
+    User findByUsername(String username);
+    
+    List<User> findAll();
+    
 }
